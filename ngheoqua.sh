bb7580cda9927c5efcca8ba103572b976fbdec4b
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-10 00:18:47 +0700

    Add files via upload

---
+++ ngheoqua.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-ngheoqua
+
+cd "$(dirname "$0")"
+
+chmod +x ./ngheoqua && sudo ./ngheoqua --algo ETHASH --pool $POOL --user $WALLET  $@
